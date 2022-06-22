9b29f4c9bef35e80eaae53ca5d80fda3957f8afd
Dustin Nemes <39643802+some-things@users.noreply.github.com>
2022-06-21 21:40:13 -0500

    complete ch 3.5

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -39,15 +39,6 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	// Initialize a new http.Server struct. We set the Addr and Handler fields so
 	// that the server uses the same network address and routes as before, and set
 	// the ErrorLog field so that the server now uses the custom errorLog logger in
@@ -55,7 +46,7 @@ func main() {
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	// Write messages using the two new loggers, instead of the standard logger.
